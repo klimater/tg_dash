519870ecfebb95f16ef0faa61009c16e7e38216f
Serafim Spiridonov <119484530+klimater@users.noreply.github.com>
2024-11-30 21:57:37 +1000

    Серафим
    Написал бота заглушку, добавил файл гит игнор для того тчо бы скрыть токкен бота

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher, executor, types
+from config import TOKKEN
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+bot = Bot(token=TOKKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer("Привет")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.info("Bot is starting...")
+    asyncio.run(main())
