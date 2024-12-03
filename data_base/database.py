87c3b8c350a36b49c2a9b308053e963cffd7bcf7
Serafim Spiridonov <119484530+klimater@users.noreply.github.com>
2024-12-03 20:23:04 +1000

    вывод ексель
    Доделал вывод ексель файла, за комментировал

--- data_base/database.py
+++ data_base/database.py
@@ -4,31 +4,32 @@ import openpyxl.reader.excel
 
 from datetime import date
 
-
-def open_exel():
+# Функция безопасного открытия файла
+def open_exel(file_name):
     try:
-        wb = openpyxl.reader.excel.load_workbook(filename= "BD.xlsx")
+        wb = openpyxl.reader.excel.load_workbook(filename= file_name)
         wb.active = 0
     except:
         print("Файл не найден")
+        return None
     
     return wb
     
-
+# Функция вывода даных из exel файла
 def output_sheet(wb):
-    dt_now = date.today()
-
-    sheet = wb.active
-
+    if wb != None:
+        sheet = wb.active
+    else:
+        return "Ошибка файл не найден"
+    value = """"""
     for row in range(1, sheet.max_row):
         date_sale = sheet[row][0].value
         name = sheet[row][1].value
         category = sheet[row][2].value
         quantity = sheet[row][3].value
         cost = sheet[row][4].value
-        print(dt_now == date_sale, str(dt_now), str(date_sale)[:10])
-        #print(date_sale)
-        #print(f"{str(date_sale)[:11]} {name} {category} {quantity} {cost}")
+        value += f"""{str(date_sale)[:11]} {name} {category} {quantity} {cost}\n"""
+    return value
 
-wb = open_exel()
-output_sheet(wb)
+wb = open_exel("BD.xlsx")
+print(output_sheet(wb))
